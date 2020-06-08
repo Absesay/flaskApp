6bf420e11b0b02ea08cd9c2b463fc720b6ca17ad
Abraham Sesay <abrahamsesay96@gmail.com>
2020-06-08 13:38:23 -0500

    setup routes and views using jinja

---
+++ data.py
@@ -0,0 +1,39 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'lorem ipsum',
+            'author': 'james brown',
+            'create_date': '06-05-2020'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'lorem ipsum',
+            'author': 'james doe',
+            'create_date': '06-05-2010'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'lorem ipsum',
+            'author': 'james brown',
+            'create_date': '06-05-2020'
+        },
+        {
+            'id': 4,
+            'title': 'Article Four',
+            'body': 'lorem ipsum',
+            'author': 'Lilie fullah',
+            'create_date': '03-18-2020'
+        },
+        {
+            'id': 5,
+            'title': 'Article Five',
+            'body': 'lorem ipsum',
+            'author': 'Queen Mei',
+            'create_date': '08-05-2020'
+        }
+    ]
+    return articles
