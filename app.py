7fe04671f0e06cfc4445b32f03ab3e6f6fb3efdc
Abraham Sesay <abrahamsesay96@gmail.com>
2020-06-08 19:45:10 -0500

    import modules for MySQL, hashing and form validation

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+# import modules
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 # flask instance
 app = Flask(__name__)
 
+# import data from database
 Articles = Articles()
 
+# routing views
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -21,6 +27,19 @@ def articles():
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('article.html', id = id)
+
+# form validation
+class RegisterForm(Form):
+    name = StringField('Name', [validators.length(min=1, max=50)])
+    userName = StringField('Username', [validators.length(min=4, max=25)])
+    email = StringField('Email', [validators.length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+
+    ])
+    confirm = PasswordField('Confirm Password')
+
 # run
 if __name__ == '__main__':
     app.run(debug=True)
